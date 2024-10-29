f001ed4393a15bdf977f1703a62f2db2b2b6df0b
M-nogales <manuelnogalesserrano5221@gmail.com>
2024-10-29 16:01:32 +0100

    hello working

--- BigData/05_Practica_Docker/test/src/main/java/com/ejemplo/App.java
+++ BigData/05_Practica_Docker/test/src/main/java/com/ejemplo/App.java
@@ -1,33 +1,18 @@
 package com.ejemplo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class App {
+public class App extends HttpServlet {
 
-    public static void main(String[] args) {
-        try {
-            // Cargar la configuración desde config.xml
-            Document config = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("config.xml");
-            config.getDocumentElement().normalize();
-            Element dbElement = (Element) config.getElementsByTagName("database").item(0);
-
-            String url = dbElement.getElementsByTagName("url").item(0).getTextContent();
-            String user = dbElement.getElementsByTagName("username").item(0).getTextContent();
-            String password = dbElement.getElementsByTagName("password").item(0).getTextContent();
-
-            // Intenta conectar con la base de datos
-            Connection connection = DriverManager.getConnection(url, user, password);
-            System.out.println("Conexión a la base de datos establecida exitosamente.");
-
-            connection.close();
-            System.out.println("Conexión cerrada.");
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
-        }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println("Hello, World!");
     }
 }
