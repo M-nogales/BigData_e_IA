1ab587f5506a361ba70afa2d3c9622772e02185d
M-nogales <manuelnogalesserrano5221@gmail.com>
2024-11-29 12:11:48 +0100

    1-6 done + schema others

--- BigData/10_consultas_Mongo/consults.js
+++ BigData/10_consultas_Mongo/consults.js
@@ -160,55 +160,148 @@ db.games.updateOne({"title":"LOL"},{$push:{"platform":"Nintendo Switch"}})
 db.games.updateMany({"rating":{$lt:8.0}},{$inc:{"rating":1}})
 
 //! Problema 4
+//! aquí descubrí que no hace falta poner "" para los campos .-.
 //1
-
+db.games.deleteOne({ title: "Fortnite" });
 //2
-//3
-//4
+db.games.updateOne({ title: "Dark Souls III" },{ $unset: { rating: "" } });
+//3 //! no hay ninguno con menos de 8, min es 8
+db.games.deleteMany({ rating: { $lt: 8.0 } });
+//4//todo size + $gt-$lt nope
+//db.games.deleteMany({ platform: { $size: { $lt: 3 } } });
+db.games.find({
+  $or: [
+    { platform: { $size: 0 } },
+    { platform: { $size: 1 } },
+    { platform: { $size: 2 } }
+  ]
+});
 //5
-//6
-//7
-//8
-/*
-db.nombre_de_tu_coleccion.deleteOne({ title: "Fortnite" });
-
-// Elimina el campo de calificación del videojuego "Dark Souls III"
-db.nombre_de_tu_coleccion.updateOne(
-  { title: "Dark Souls III" },
-  { $unset: { rating: "" } }
-);
-
-// Elimina todos los videojuegos que tengan un rating inferior a 8.0
-db.nombre_de_tu_coleccion.deleteMany({ rating: { $lt: 8.0 } });
-
-// Elimina todos los videojuegos que tengan menos de 3 plataformas
-db.nombre_de_tu_coleccion.deleteMany({ platform: { $size: { $lt: 3 } } });
-
-// Elimina todos los videojuegos que sean del género "MOBA"
-db.nombre_de_tu_coleccion.deleteMany({ genre: "MOBA" });
-
-// Elimina el campo de género de todos los videojuegos que tengan un rating inferior a 8.0
-db.nombre_de_tu_coleccion.updateMany(
+db.games.deleteMany({ genre: "MOBA" });
+//6 //!no hay ninguno con menos de 8
+db.games.updateMany(
   { rating: { $lt: 8.0 } },
   { $unset: { genre: "" } }
 );
-
-// Elimina todos los videojuegos lanzados antes de 2010
-db.nombre_de_tu_coleccion.deleteMany({ releaseYear: { $lt: 2010 } });
-
-// Elimina el videojuego con el menor número de plataformas
-db.nombre_de_tu_coleccion.find().sort({ platform: 1 }).limit(1).forEach(function(doc) {
-  db.nombre_de_tu_coleccion.deleteOne({ _id: doc._id });
-});
-*/
+//7
+db.games.deleteMany({ releaseYear: { $lt: 2010 } });
+//8 //todo
 
 //! Problema 5
+//1
+db.games.createIndex({ title: "text" });
+//2
+db.games.createIndex({ genre: 1, rating: 1 },{name:"genders and ratings"});
+//3
+db.games.createIndex({ title: -1, releaseYear: 1 });
+//4 //? todo
+db.games.createIndex({ platform: text });
+// MongoServerError[IndexOptionsConflict]: An equivalent index already exists with a different name and options. Requested index: { v: 2, key: { _fts: "text", _ftsx: 1 }, name: "platform_text", weights: { platform: 1 }, default_language: "english", language_override: "language", textIndexVersion: 3 }, existing index: { v: 2, key: { _fts: "text", _ftsx: 1 }, name: "title_text", weights: { title: 1 }, default_language: "english", language_override: "language", textIndexVersion: 3 }
+
+//* db.games.getIndexes() | db.games.getIndices()
+
+//! Problema 6
+//0
+db.users.insertMany([{
+  username: "SuperCoder123",
+  first_name: "Super",
+  last_name: "Coder"
+},
+{
+  username: "TechGuru99",
+  full_name: {
+    first: "Tech",
+    last: "Guru"
+  }
+}])
+//
+db.users.insertMany([{
+  username: "SuperCoder123",
+  first_name: "Super",
+  last_name: "Coder"
+},
+{
+  username: "TechGuru99",
+  full_name: {
+    first: "Tech",
+    last: "Guru"
+  }
+}])
+db.posts.insertMany([{
+  username: "SuperCoder123",
+  title: "Solves a coding challenge",
+  body: "Optimizes the algorithm and achieves maximum efficiency."
+},
+{
+  username: "SuperCoder123",
+  title: "Shares coding tutorials",
+  body: "Helps aspiring coders with step-by-step guides and examples."
+},
+{
+  username: "TechGuru99",
+  title: "Discovers a software vulnerability",
+  body: "Reports it to the developers for prompt fixing."
+},
+{
+  username: "TechGuru99",
+  title: "Creates an innovative tech product",
+  body: "Introduces a groundbreaking invention to simplify everyday tasks"
+}])
+// para saber los ids de los posts dados los titulos
+db.posts.find({ title: "Solves a coding challenge" }, { _id: 1 });
+//
+db.comments.insertMany([
+  {
+    username: "SuperCoder123",
+    comment: "Hope you got a good deal!",
+    post: ObjectId("674996f5eb04addc01260af3")
+  },
+  {
+    username: "SuperCoder123",
+    comment: "What's mine is yours!",
+    post: ObjectId("674996f5eb04addc01260af5")
+  },
+  {
+    username: "SuperCoder123",
+    comment: "Don't violate the licensing agreement!",
+    post: ObjectId("674996f5eb04addc01260af4")
+  },
+  {
+    username: "TechGuru99",
+    comment: "It still isn't clean",
+    post: ObjectId("674996f5eb04addc01260af3")
+  },
+  {
+    username: "TechGuru99",
+    comment: "Denied your PR because I found a hack",
+    post: ObjectId("674996f5eb04addc01260af4")
+  }
+]);
+//1
+db.users.find()
+//2
+db.posts.find()
+//3
+db.users.find({ username: "SuperCoder123" })
+//4
+db.posts.find({ username: "TechGuru99" })
+//5
+db.comments.find()
+//6
+db.comments.find({ username: "SuperCoder123" })
+//7
+db.comments.find({ username: "TechGuru99" })
+//8
+db.users.find({ title: "Shares coding tutorials" }, { _id: 1 });
+db.comments.find({ post: ObjectId("674996f5eb04addc01260af4") });
+
+//! 7
 //1
 //2
 //3
 //4
 
-//! Problema 6
+//!8
 //1
 //2
 //3
@@ -217,3 +310,42 @@ db.nombre_de_tu_coleccion.find().sort({ platform: 1 }).limit(1).forEach(function
 //6
 //7
 //8
+//9
+
+//!9
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+
+//!10
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+
+//!11
+//1
+//2
+//3
+//4
+//5
+//6
+
+//!12
+//1
+//2
+//3
+//4
+//5
+//6
