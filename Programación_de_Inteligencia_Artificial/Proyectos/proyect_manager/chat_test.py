e01041aff4e9fff9c0b163b261afde8a2930bcef
M-nogales <manuelnogalesserrano5221@gmail.com>
2025-05-05 19:24:04 +0200

    working gemini code

--- Programación_de_Inteligencia_Artificial/Proyectos/proyect_manager/chat_test.py
+++ Programación_de_Inteligencia_Artificial/Proyectos/proyect_manager/chat_test.py
@@ -1,18 +1,18 @@
-import os
-from openai import OpenAI
+# import os
+# from openai import OpenAI
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key="secret api key",
-)
+# client = OpenAI(
+#     # This is the default and can be omitted
+#     api_key="secret api key",
+# )
 
-response = client.responses.create(
-    model="gpt-4o",
-    instructions="You are a weather assistant. Provide a brief weather report.",
-    input="what's the weather in seville today?",
-)
+# response = client.responses.create(
+#     model="gpt-4o",
+#     instructions="You are a weather assistant. Provide a brief weather report.",
+#     input="what's the weather in seville today?",
+# )
 
-print(response.output_text)
+# print(response.output_text)
 #----
 
 # from openai import OpenAI
@@ -31,3 +31,20 @@ print(response.output_text)
 # )
 
 # print(completion.choices[0].message.content)
+#! gemini: 
+'''
+15 RPM	Máximo de 15 solicitudes por minuto (Requests Per Minute).
+1,000,000 TPM	Máximo de 1 millón de tokens por minuto (Tokens Per Minute).
+1,500 RPD	Máximo de 1,500 solicitudes por día (Requests Per Day).
+'''
+
+from google import genai
+
+client = genai.Client(api_key="GEMINI_KEY")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
